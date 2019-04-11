870b4064d2f57c158c8cf779fccea5be9c6b3736
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-04-11 07:18:42 +0430

    Update ModelTrainer

--- code/ModelTrainer.py
+++ code/ModelTrainer.py
@@ -3,8 +3,27 @@ import sys
 
 
 def main():
-    model_to_train = sys.argv[1]
-    if model_to_train == "PCA":
+    if len(sys.argv) == 1:
+        model_to_train = "all"
+    else:
+        model_to_train = sys.argv[1]
+    if model_to_train == "all":
+        command = "python ./vec_arth_pca.py"
+        subprocess.call([command], shell=True)
+
+        command = "python ./vec_arth.py"
+        subprocess.call([command], shell=True)
+
+        command = "python ./st_gan.py train"
+        subprocess.call([command], shell=True)
+
+        command = "python ./train_cvae.py"
+        subprocess.call([command], shell=True)
+
+        command = "python ./train_scGen.py"
+        subprocess.call([command], shell=True)
+
+    elif model_to_train == "PCA":
         command = "python ./vec_arth_pca.py"
         subprocess.call([command], shell=True)
     elif model_to_train == "VecArithm":
